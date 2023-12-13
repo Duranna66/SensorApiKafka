568d5165ac715eb63742f934ed659d735630cef1
Duranna66 <ivan.kushnarev.04@list.ru>
2023-12-13 15:06:31 +0300

    add conf

---
+++ src/main/java/com/example/RestApiSensor/RestApiSensorApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestApiSensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSensorApplication.class, args);
+	}
+
+}
