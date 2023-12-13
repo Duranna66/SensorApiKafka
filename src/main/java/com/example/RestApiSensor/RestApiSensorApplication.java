53b8d811ca9da3078f7d5e8eb32f4d05ef6ddad2
Duranna66 <ivan.kushnarev.04@list.ru>
2023-12-13 21:55:23 +0300

    test

--- src/main/java/com/example/RestApiSensor/RestApiSensorApplication.java
+++ src/main/java/com/example/RestApiSensor/RestApiSensorApplication.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Bean;
 public class RestApiSensorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApiSensorApplication.class, args);
+		SpringApplication.run(RestApiSensorApplication.class, args);//test
 	}
 	@Bean
 	public ModelMapper modelMapper() {
